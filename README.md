ed3444e6df50b90d69a18b45a6075867d90e6101
David Faveris <David Faveris>
2016-09-30 07:17:51 +0200

    nodejs server with socket.io and Polymer

---
+++ README.md
@@ -0,0 +1,25 @@
+# Pa-Comp
+
+```
+git clone https://github.com/scenaristeur/Pa-Comp.git Pa-Comp
+cd Pa-Comp
+npm install
+cd public
+bower install
+cd ..
+node . 
+```
+
+Le serveur doit être accessible à l'adresse http://127.0.0.1:3000
+
+Contact :
+twitter : @dfaveris #smag0
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,24 @@
+// Setup basic express server
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+	console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+//Socket.IO
+io.on('connect', function(){
+	console.log("connecte");
+});
+io.on('event', function(data){
+	console.log(data);
+});
+io.on('disconnect', function(){
+	console.log("deconnecte");
+});
